f05dc59a926704642630c49c55aa612fb4a398dd
Matic <matic.sulc@sers.si>
2020-07-03 14:37:11 +0200

    https

--- src/app/services/country-data.service.ts
+++ src/app/services/country-data.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Country } from '../models/country.model';
 
-const API_URL = "http://api.worldbank.org/v2/country/?format=json";
+const API_URL = "https://api.worldbank.org/v2/country/?format=json";
 
 @Injectable()
 export class CountryDataService {
